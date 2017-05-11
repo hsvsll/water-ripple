798dba3f774adb6dbee73c2aeb72d43345f7f826
hsvsll <wdyj0.0@foxmail.com>
2017-05-11 15:57:44 +0800

    水波纹效果

---
+++ WaterRipple/app/src/main/java/com/practice/hs/waterripple/MyWaterRippleLayout.java
@@ -0,0 +1,24 @@
+package com.practice.hs.waterripple;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by huha on 2017/5/11.
+ * 水波纹实现父布局
+ */
+
+public class MyWaterRippleLayout extends LinearLayout{
+    public MyWaterRippleLayout(Context context) {
+        super(context);
+    }
+
+    public MyWaterRippleLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyWaterRippleLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
